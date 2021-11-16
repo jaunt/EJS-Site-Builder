8228c25fedabc51678729edbcfba41be3df9d01c
Stephen Evans <steve@singdaptive.com>
2021-11-15 21:19:04 -0800

    Removed data that could change and still be valid in advanced test.

--- tests/advanced/templates/postGenerate.js
+++ tests/advanced/templates/postGenerate.js
@@ -5,17 +5,25 @@
 // or reject on failure.
 //
 // Inputs:
-//  outData -> information about all files written to output
+//  output -> information about all files written to output
 //
 // Outputs:
 //
 // site: list of json serializable objects which will created as json files
 //            for the purposes of loading in to the web site's runtime javascript code
 
+// hack to make sure we get the same output on every test
+let all = [];
+for (let key in output) {
+  all.push(...Object.entries(output[key]));
+}
+all = all.map((pair) => {
+  return { page: pair[0], path: pair[1] };
+});
+all.sort((a, b) => a.page.localeCompare(b.page));
+
 resolve({
   siteFiles: {
-    "multipage.json": {
-      output,
-    },
+    "multipage.json": all,
   },
 });
--- tests/advanced/templates/preGenerate.js
+++ tests/advanced/templates/preGenerate.js
@@ -14,7 +14,7 @@
 //
 // site: list of json serializable objects which will created as json files
 //            for the purposes of loading in to the web site's runtime javascript code
-const now = new Date().toISOString();
+const now = "2021-nov-15";
 const version = 1.4;
 
 const toLowerCase = function (text) {
